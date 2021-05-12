6a2f5409db85ac62bbe4f2a96c39a9712164455d
shanep <shanecandoit@gmail.com>
2021-05-12 13:45:17 -0500

    tests pass

--- pythagorean_triplet.go
+++ pythagorean_triplet.go
@@ -2,6 +2,7 @@ package pythagorean
 
 import "fmt"
 
+// Triplet 3 ints, ideally a<b<c, a**a+b**b == c**c
 type Triplet [3]int
 
 // Pythagorean triplet is a set of three natural numbers, {a, b, c},
@@ -18,13 +19,13 @@ func Range(min, max int) []Triplet {
 
 	results := []Triplet{}
 
-	for a := min; a < max; a++ {
-		for b := a + 1; b < max; b++ {
-			for c := b + 1; c < max; c++ {
+	for a := min; a <= max; a++ {
+		for b := a; b <= max; b++ {
+			for c := b; c <= max; c++ {
 				//fmt.Println("a", a, "b", b, "c", c)
 
 				if a*a+b*b == c*c {
-					fmt.Println("valid a", a, "b", b, "c", c, "=", a*a, b*b, c*c)
+					// fmt.Println("valid a", a, "b", b, "c", c, "=", a*a, b*b, c*c)
 					results = append(results, Triplet{a, b, c})
 				}
 
@@ -36,6 +37,25 @@ func Range(min, max int) []Triplet {
 	return results
 }
 
+// Sum returns a list of all Pythagorean triplets where the sum a+b+c == p
 func Sum(p int) []Triplet {
-	return nil
+	allTriplets := Range(1, p)
+
+	results := []Triplet{}
+
+	// filter on a+b+c==p
+	for _, trip := range allTriplets {
+
+		a, b, c := trip[0], trip[1], trip[2]
+		if a+b+c == p {
+			fmt.Println("triplet", trip)
+			results = append(results, trip)
+		}
+	}
+
+	fmt.Println("Sum", 1, p, "#", len(results))
+	// for i, res := range results {
+	//	fmt.Println(" ", i, "\t", res)
+	// }
+	return results
 }
